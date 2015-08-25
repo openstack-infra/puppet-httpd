3bcc9171dcec301a74e7a5c9e95a154c9e082fc4
Spencer Krum <nibz@spencerkrum.com>
2015-08-25 11:52:02 -0700

    Create a httpd::mod defined type
    This allows for before => Service['httpd'] on every resource without
    being explicit.
    
    Change-Id: I0e991a7a63317e608cc6ef35b58dc79b518ba863

---
+++ manifests/mod.pp
@@ -0,0 +1,12 @@
+# defined type to wrap httpd_mod
+# httpd_mod doesn't autobefore the httpd service, this does
+define httpd::mod (
+) {
+
+  httpd_mod { $name':
+    ensure => present;
+    before => Service['httpd'],
+  }
+
+
+}
