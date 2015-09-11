d2f2c0c6488d2fd3940b7540788f7b07fc638e2b
Glauco Oliveira <gvinici@thoughtworks.com>
2015-09-11 11:23:06 -0300

    Add acceptance tests for puppet-httpd
    Add acceptance tests for puppet-httpd module so that once the module is
    applied we check if files were created, packages were installed and
    services were started.
    
    Change-Id: I3e1430f2937bd2bc5ea789c626784af7d5d85959
    Co-Authored-By: Bruno Tavares <btavare@thoughtworks.com>
    Co-Authored-By: Danilo Ramalho <dramalho@thoughtworks.com>

---
+++ spec/acceptance/packages_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper_acceptance'
+
+describe 'required packages' do
+  describe 'RedHat packages', :if => ['fedora', 'redhat'].include?(os[:family]) do
+    required_packages = [
+      package('httpd'),
+      package('httpd-devel'),
+      package('php'),
+      package('mod_ssl'),
+    ]
+
+    required_packages.each do |package|
+      describe package do
+        it { should be_installed }
+      end
+    end
+  end
+
+  describe 'Debian packages', :if => ['debian', 'ubuntu'].include?(os[:family]) do
+    required_packages = [
+      package('apache2'),
+      package('apache2-dev'),
+      package('libaprutil1-dev'),
+      package('libapr1-dev'),
+      package('libapache2-mod-php5'),
+      package('libapache2-mod-python'),
+    ]
+
+    required_packages.each do |package|
+      describe package do
+        it { should be_installed }
+      end
+    end
+  end
+end
