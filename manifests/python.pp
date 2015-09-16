1b056fd625343fc163a179ea7a23b4d49cb2d941
Bruno Tavares <btavare@thoughtworks.com>
2015-09-16 11:06:33 -0300

    Add note of support on Python mods for CentOS 7.
    CentOS 7 has offically deprecated mod_python in favor of mod_wsgi and
    the package is not available anymore. This change add a note on the
    module about the offically supported way to run python applications on
    CentOS 7.
    
    Change-Id: I9bf2e0dde6575f50db6ce12abf896b5890b42419
    Co-Authored-By: Danilo Ramalho <dramalho@thoughtworks.com>

--- manifests/python.pp
+++ manifests/python.pp
@@ -1,6 +1,7 @@
 # Class: httpd::python
 #
 # This class installs Python for Apache
+# Please use httpd::mod::wsgi for CentOS >= 7 as mod_python is officially deprecated.
 #
 # Parameters:
 # - $python_package
