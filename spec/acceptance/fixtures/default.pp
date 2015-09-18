cd7241ba7d080507aa56bc7f4bf78bf1032412cc
Bruno Tavares <btavare@thoughtworks.com>
2015-09-18 17:27:57 -0300

    Fix proxy and redirect configuration file names.
    While working on puppet-lodgeit acceptance tests we found that the
    configuration file that `httpd::mod::proxy` creates was not being picked
    up by Apache because it was missing the prefix `.conf`.
    
    This change has fixes for `httpd::mod::proxy` and `httpd::mod::redirect`
    as they have the same issue. We added tests as well to increase the
    confidence on the fix.
    
    Change-Id: I82241038d687316f91f18209fe8323c12422e2f8
    Co-Authored-By: Danilo Ramalho <dramalho@thoughtworks.com>

--- spec/acceptance/fixtures/default.pp
+++ spec/acceptance/fixtures/default.pp
@@ -10,6 +10,43 @@ httpd::vhost { 'localhost':
   redirect_ssl => true,
 }
 
+# Enable a secondary port to test proxy and redirect modules
+$override = '
+Listen 8080
+<Directory "/tmp">
+	Options All
+	AllowOverride All
+	Require all granted
+</Directory>
+'
+file { "${::httpd::params::vdir}override.conf":
+  content => $override,
+}
+file { '/tmp/acceptance.txt':
+  ensure  => present,
+  owner   => $::httpd::params::user,
+  group   => $::httpd::params::group,
+  content => 'Acceptance Test',
+}
+httpd::vhost { 'acceptance-server':
+  servername => 'localhost',
+  port       => 8080,
+  docroot    => '/tmp',
+  priority   => 50,
+}
+
+httpd::mod { 'proxy': ensure => present; }
+httpd::mod { 'proxy_http': ensure => present; }
+httpd::vhost::proxy { 'proxy':
+  port => 80,
+  dest => 'http://localhost:8080',
+}
+
+httpd::vhost::redirect { 'redirect':
+  port => 80,
+  dest => 'http://localhost:8080/acceptance.txt',
+}
+
 httpd::mod { 'rewrite':
   ensure => present,
 }
