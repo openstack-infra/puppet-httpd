8c865e68ab21fcb02c726a2575e85e4bbe78f08a
Bruno Tavares <btavare@thoughtworks.com>
2015-09-18 17:27:57 -0300

    Fix proxy and redirect configuration file names.
    While working on puppet-lodgeit acceptance tests we found that the
    configuration file that `httpd::mod::proxy` creates was not being picked
    up by Apache because it was missing the prefix `.conf`.
    
    This change has fixes for `httpd::mod::proxy` and `httpd::mod::redirect`
    as they have the same issue. We added tests as well to increase the
    confidence on the fix.
    
    Change-Id: I82241038d687316f91f18209fe8323c12422e2f8
    Co-Authored-By: Danilo Ramalho <dramalho@thoughtworks.com>

--- manifests/vhost/redirect.pp
+++ manifests/vhost/redirect.pp
@@ -30,8 +30,8 @@ define httpd::vhost::redirect (
 
   $srvname = $name
 
-  file { "${priority}-${name}":
-    path    => "${httpd::params::vdir}/${priority}-${name}",
+  file { "${priority}-${name}.conf":
+    path    => "${httpd::params::vdir}/${priority}-${name}.conf",
     content => template($template),
     owner   => 'root',
     group   => 'root',
