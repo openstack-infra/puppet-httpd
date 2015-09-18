3abc4f4fdf87e2cc09c08d14d13af3243634a68c
Bruno Tavares <btavare@thoughtworks.com>
2015-09-18 17:27:57 -0300

    Fix proxy and redirect configuration file names.
    While working on puppet-lodgeit acceptance tests we found that the
    configuration file that `httpd::mod::proxy` creates was not being picked
    up by Apache because it was missing the prefix `.conf`.
    
    This change has fixes for `httpd::mod::proxy` and `httpd::mod::redirect`
    as they have the same issue. We added tests as well to increase the
    confidence on the fix.
    
    Change-Id: I82241038d687316f91f18209fe8323c12422e2f8
    Co-Authored-By: Danilo Ramalho <dramalho@thoughtworks.com>

--- spec/acceptance/fixtures/default.pp
+++ spec/acceptance/fixtures/default.pp
@@ -10,6 +10,18 @@ httpd::vhost { 'localhost':
   redirect_ssl => true,
 }
 
+httpd::mod { 'proxy': ensure => present; }
+httpd::mod { 'proxy_http': ensure => present; }
+httpd::vhost::proxy { 'openstack-proxy':
+  port => 80,
+  dest => 'http://review.openstack.org',
+}
+
+httpd::vhost::redirect { 'openstack-redirect':
+  port => 80,
+  dest => 'http://www.openstack.org',
+}
+
 httpd::mod { 'rewrite':
   ensure => present,
 }
