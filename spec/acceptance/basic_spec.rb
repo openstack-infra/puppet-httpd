3daa0b44acdc6c9d2e30a8a2dab09a8123fdd569
Bruno Tavares <btavare@thoughtworks.com>
2015-09-18 17:27:57 -0300

    Fix proxy and redirect configuration file names.
    While working on puppet-lodgeit acceptance tests we found that the
    configuration file that `httpd::mod::proxy` creates was not being picked
    up by Apache because it was missing the prefix `.conf`.
    
    This change has fixes for `httpd::mod::proxy` and `httpd::mod::redirect`
    as they have the same issue. We added tests as well to increase the
    confidence on the fix.
    
    Change-Id: I82241038d687316f91f18209fe8323c12422e2f8
    Co-Authored-By: Danilo Ramalho <dramalho@thoughtworks.com>

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -112,5 +112,20 @@ describe 'puppet-httpd module' do
     describe command('a2query -m ssl'), :if => ['debian', 'ubuntu'].include?(os[:family]) do
       its(:stdout) { should match 'enabled' }
     end
+
+    describe 'vhosts' do
+      describe command('curl --verbose http://localhost') do
+        its(:stdout) { should include 'Index of /' }
+      end
+
+      describe command('curl --verbose -H "Host: proxy" http://localhost/acceptance.txt') do
+        its(:stdout) { should include 'Acceptance Test' }
+      end
+
+      describe command('curl --verbose -H "Host: redirect" http://localhost') do
+        its(:stdout) { should include '302' }
+        its(:stdout) { should include 'http://localhost:8080/acceptance.txt' }
+      end
+    end
   end
 end
