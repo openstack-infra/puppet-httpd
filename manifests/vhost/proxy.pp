3d9f1e8e210f9bdd275ad8737053831fe48237b0
Yolanda Robla <yroblamo@redhat.com>
2016-06-24 14:01:53 +0200

    Fix selinux problems on vhost
    When running on CentOS, two problems appeared:
    - when using proxy, apache was failing with an error 500,
    fixed by enabling httpd_can_network_connect
    
    - when trying to access files under a vhost, apache was
    failing with Access denied because search permissions
    are missing. So running chcon on the docroot for each vhost
    
    Change-Id: I87c4c0e51f05eab8f5c8e094c2c54504e60b97af

--- manifests/vhost/proxy.pp
+++ manifests/vhost/proxy.pp
@@ -66,5 +66,11 @@ define httpd::vhost::proxy (
     notify  => Service['httpd'],
   }
 
-
+  # enable that setting, that allows httpd scripts and
+  # modules to connect to the network
+  if $::osfamily == 'RedHat' {
+    selinux::boolean { 'httpd_can_network_connect':
+      ensure => "on",
+    }
+  }
 }
